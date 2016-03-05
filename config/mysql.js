9970810fea97845189c9790f65b80d148b0b9d04
Kris Ekenes <krisekenes@Kriss-MacBook-Pro.local>
2016-03-05 15:42:27 -0800

    mysql is conencted

--- config/mysql.js
+++ config/mysql.js
@@ -6,12 +6,12 @@ var connection = mysql.createConnection({
   database : 'commonwealth'
 });
 
-connection.connect();
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+  console.log('connected as id ' + connection.threadId);
 });
 
-connection.end();
