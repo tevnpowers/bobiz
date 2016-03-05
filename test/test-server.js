6190a128727236aaad69ca89dbd7fb06338a5d97
jon madison <jon@tfftech.com>
2016-03-05 15:08:50 -0800

    add business

--- test/test-server.js
+++ test/test-server.js
@@ -10,17 +10,30 @@ var request = require('supertest');
 var qs = require('qs');
 
 describe('Business API GET', function() {
-  it('returns posts from', function(done) {
+  it('returns business info from repo', function(done) {
     .get('/api/businesses/')
     .expect(200)
     .end(function(err,res) {
       expect(err).to.be.null();
       console.log('body: ' + JSON.stringify(res));
       res.body.Name.must.equal("MINI LOVE ARROWS");
-      res.body.StyleId.must.equal(4227420);
-      res.body.Price.must.equal("2.00");
+      res.body.Address.must.equal(4227420);
+      res.body.Hours.must.equal("2.00");
+      res.body.Social.must.equal("2.00");
+      res.body.Phone.must.equal("2.00");
+      res.body.Website.must.equal("2.00");
+      res.body.Category.must.equal("2.00");
+      res.body.ProfileImage.must.equal("2.00");
+      res.body.DaysOpen.must.equal("2.00");
+      res.body.Descriptions.must.equal("2.00");
+      res.body.Email.must.equal("2.00");
+      res.body.Status.must.equal("2.00");
       done();
     });
   });
 });
+
+describe('Reviews API GET', function() {
+  it('returns reviews from ')
+}
 
