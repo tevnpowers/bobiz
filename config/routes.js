377dd56306936db96426f9f1f539211f854817aa
jon madison <jon@tfftech.com>
2016-03-05 15:43:25 -0800

    added routes as skeleton for endpoints

--- config/routes.js
+++ config/routes.js
@@ -2,11 +2,24 @@
 
 var global = require('./config');
 var posts = require('../app/controllers/businesses');
+var reviews = require('../app/controllers/reviews');
 
 module.exports = function(app) {
   app.get(global.apiPrefix + '/businesses',
     function(req, res) {
       posts.index.json(req, res);
+    }
+  );
+
+  app.get(global.apiPrefix + '/businesses/:id',
+    function(req, res) {
+      posts.index.json(req, res);
+    }
+  );
+
+  app.get(global.apiPrefix + '/businesses/:id/reviews',
+    function(req, res) {
+      reviews.index.json(req, res);
     }
   );
 };
