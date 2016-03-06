6258bb95e037585209d9d43e80f372c9f51bef3f
jon madison <jon@tfftech.com>
2016-03-06 01:40:30 -0800

    find and findone

--- app/controllers/businesses.js
+++ app/controllers/businesses.js
@@ -1,7 +1,31 @@
 'use strict';
 
+var mongoose = require('mongoose');
+var Business = mongoose.model('Business');
+
+exports.findOne = {
+  json: function findOneJson(req,res) {
+    console.log("looking for: " + req.params.id)
+    var result = []
+    Business.findById(req.params.id,function(err,biz){
+      if(err) {
+        console.log("error:" + err)
+        return res.status(500).send(err)
+      }
+
+      if(biz == undefined) {
+        return res.status(404).send({"error":"not found."})
+      }
+
+      res.status(200).send(biz)
+    })
+  }
+}
+
 exports.index = {
     json: function(req, res) {
-
+        Business.find({}, function(err, businesses) {
+            res.status(200).send(businesses);
+        });
     }
 };
--- config/routes.js
+++ config/routes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
 
   app.get(global.apiPrefix + '/businesses/:id',
     function(req, res) {
-      posts.index.json(req, res);
+      posts.findOne.json(req, res);
     }
   );
 
